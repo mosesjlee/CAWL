1d03b5b052dd40e9b1e3d7e1f686f9f7704cd835
Moses Lee <mosesjlee@gmail.com>
2017-11-01 22:52:11 -0700

    Updated some UI stuff

--- CAWL/CAWLModDelayEffect.cpp
+++ CAWL/CAWLModDelayEffect.cpp
@@ -7,3 +7,33 @@
 //
 
 #include "CAWLModDelayEffect.hpp"
+
+void CAWLModDelayEffect::processBuffer(float * buf, const unsigned int numSamples)
+{
+    double xCurrSample = 0.0;
+    double zDelayedSample = 0.0;
+    double xHCurrSample = lastSampleOfBlock;
+    double yCurrOutput = 0.0;
+    for(int i = 0; i < numSamples; i++)
+    {
+        if(debugCounter == 44)
+            printf("STOP\n");
+        xCurrSample = buf[i];
+        zDelayedSample = delayLine->processNextSample(xHCurrSample);
+        //zDelayedSample = delayHsu->tick(xHCurrSample);
+        xHCurrSample = xCurrSample + zDelayedSample * mFeedbackGain;
+        yCurrOutput = zDelayedSample * mFeedForwardGain + xHCurrSample * mMixLevel;
+        buf[i] = yCurrOutput + xCurrSample * dryMix;
+        buf[i] = xCurrSample + yCurrOutput * mFeedForwardGain;
+        
+        delayLine->setDelayTimeInMilliseconds(flangedValue());
+        //delayHsu->setDelayLineDelay(flangedValue() * 44100.0/1000.0);
+        debugCounter++;
+    }
+    lastSampleOfBlock = xHCurrSample;
+}
+
+double CAWLModDelayEffect::flangedValue()
+{
+    return modDepth * fabs(sine->getNextSample());
+}
--- CAWL/CAWLModDelayEffect.hpp
+++ CAWL/CAWLModDelayEffect.hpp
@@ -8,7 +8,22 @@
 
 #ifndef CAWLModDelayEffect_hpp
 #define CAWLModDelayEffect_hpp
+#include "CAWLSineWaveOsc.hpp"
+#include "CAWLUniversalCombFilter.hpp"
 
-#include <stdio.h>
+class CAWLModDelayEffect : public CAWLUniversalCombFilter
+{
+public:
+    void setModulationDepth(double newModDepth);
+    void setMixLevel(double mixLevel);
+    void setModulationSpeed(double newModSpeed);
+    void processBuffer(float * buf, const unsigned int numSamples);
+    double flangedValue();
+private:
+    CAWLSineWaveOsc * sine;
+    double modDepth;
+    double modSpeed;
+    double mixLevel;
+};
 
 #endif /* CAWLModDelayEffect_hpp */
